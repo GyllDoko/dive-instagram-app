d8fcf9c123527b2a6bbb23c9982e27819b2c3c3d
DOKO ALFA Gyll-christ <dokogyll@gmail.com>
2021-07-23 15:55:49 +0100

    adding favorites pages

--- app/controllers/favorite_controller.rb
+++ app/controllers/favorite_controller.rb
@@ -1,5 +1,8 @@
 class FavoriteController < ApplicationController
-        
+    def index
+        @favorites = current_user.favorites.all 
+        puts @favorites
+    end
     def create
         favorite = current_user.favorites.create(post_id: params[:post_id])
         redirect_to post_path(params[:post_id]), notice: "#{favorite.post.user.name}, ce post a été ajouté a vos favoris"
